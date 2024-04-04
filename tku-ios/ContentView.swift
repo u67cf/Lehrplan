0e1df146fd21fdbdde0aa0d0873efa43efd4e1e3
bochain <53370227+cantpr09ram@users.noreply.github.com>
2024-04-04 23:09:13 +0800

    feat: add browse for iPad version

--- tku-ios/ContentView.swift
+++ tku-ios/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
-
+    @State private var isContainersSectionExpanded: Bool = true
     var body: some View {
         if horizontalSizeClass == .regular {
             // iPad and some iPhone models in landscape might have .regular horizontal size class
@@ -29,9 +29,61 @@ struct ContentView: View {
                 NavigationLink("Summary", destination: SummaryView())
                 
                 NavigationLink("Account", destination: AccountView())
+                
+                DisclosureGroup("Browse", isExpanded: $isContainersSectionExpanded) {
+                    NavigationLink(destination: ClassScheduleView()) {
+                        Label(
+                            title: { Text("課表") },
+                            icon: { Image(systemName: "calendar.day.timeline.left")}
+                        )
+                    }
+                    NavigationLink(destination: CalendarView()) {
+                        Label(
+                            title: { Text("行事曆") },
+                            icon: { Image(systemName: "calendar")}
+                        )
+                    }
+                    NavigationLink(destination: PassView()) {
+                        Label(
+                            title: { Text("圖書館") },
+                            icon: { Image(systemName: "books.vertical.fill") }
+                        )
+                    }
+                    NavigationLink(destination: CalendarView()) {
+                        Label(
+                            title: { Text("無線網路") },
+                            icon: { Image(systemName: "wifi")}
+                        )
+                    }
+                    NavigationLink(destination: CalendarView()) {
+                        Label(
+                            title: { Text("動態資訊") },
+                            icon: { Image(systemName: "info.circle.fill")}
+                        )
+                    }
+                    NavigationLink(destination: CalendarView()) {
+                        Label(
+                            title: { Text("安全校園") },
+                            icon: { Image(systemName: "shield.lefthalf.filled")}
+                        )
+                    }
+                    NavigationLink(destination: CalendarView()) {
+                        Label(
+                            title: { Text("校園資訊") },
+                            icon: { Image(systemName: "map.fill")}
+                        )
+                    }
+                    NavigationLink(destination: CalendarView()) {
+                        Label(
+                            title: { Text("淡江時報") },
+                            icon: { Image(systemName: "newspaper.fill")}
+                        )
+                    }
+                }
+                    
             }
             .navigationTitle("Menu")
-            
+            .listStyle(SidebarListStyle())
         }
     }
 
